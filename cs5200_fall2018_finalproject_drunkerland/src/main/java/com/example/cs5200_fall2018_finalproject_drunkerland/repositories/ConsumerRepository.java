6c88d706234392e84b6a3ac33916558b3c753899
Zhang.zil <cliviazhangzl@gmail.com>
2018-11-23 15:35:46 -0500

    rename consumer

--- cs5200_fall2018_finalproject_drunkerland/src/main/java/com/example/cs5200_fall2018_finalproject_drunkerland/repositories/ConsumerRepository.java
+++ cs5200_fall2018_finalproject_drunkerland/src/main/java/com/example/cs5200_fall2018_finalproject_drunkerland/repositories/ConsumerRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.cs5200_fall2018_finalproject_drunkerland.models.*;
 
-public interface CustomerRepository extends CrudRepository<Customer, Integer>{
+public interface ConsumerRepository extends CrudRepository<Consumer, Integer>{
 
 }
